ed6acd2955eaadefb3c065897d4763ba034a4aa8
Diego HO <diegogit11@gmail.com>
2024-09-23 10:30:10 -0600

    remove ts eslint warnings by adding package

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "react-calendu" {
+  const DirectEmbed: any;
+  const FloatingButton: any;
+  const InlineText: any;
+  const CallToActionButton: any;
+  export { DirectEmbed, FloatingButton, InlineText, CallToActionButton };
+}
